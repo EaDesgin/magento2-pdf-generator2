a831f88d2c47bd7ec8b5e444c7f1586ddd260e14
Lucian Miculescu <eadesignro@users.noreply.github.com>
2016-07-28 14:13:33 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -17,8 +17,6 @@
  * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
-<?php
-
 use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
